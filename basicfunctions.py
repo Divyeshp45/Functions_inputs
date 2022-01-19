a6c032bae9368caf5863770cbd1c7a4410163c1e
Divyesh <divyeshcoder452@gmail.com>
2022-01-19 12:56:57 +0530

    learned functions

---
+++ basicfunctions.py
@@ -0,0 +1,9 @@
+#HERE I CREATED BASIC MULTIPLY FUNCTION WHICH TAKES THREE ARGUMENT
+def multiply(x,y,z):
+    var=x*3
+    var1=y*2
+    var2=z*8
+    print(f"{var},{var1},{var2}")
+
+multiply(3,2,2)    
+multiply(y=3,x=2,z=1)
